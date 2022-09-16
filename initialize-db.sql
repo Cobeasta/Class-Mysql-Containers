f3481c8162d99969da110b70e40369a4e89a99a3
Cobeasta <stopyloknok@gmail.com>
2022-09-16 14:20:50 -0500

    playing with a DDL script

--- initialize-db.sql
+++ initialize-db.sql
@@ -1,18 +1,20 @@
+
 CREATE DATABASE  IF NOT EXISTS Schedule_Planner;
 USE Schedule_Planner;
 
-DROP TABLE IF EXISTS People;
+-- DROP TABLE IF EXISTS People;
 
-CREATE TABLE IF NOT EXISTS People(
-id INT NOT NULL,
-first_name varchar(255),
-last_name VARCHAR (255),
-PRIMARY KEY (id)
-);
+--
+-- CREATE TABLE IF NOT EXISTS People(
+-- id INT NOT NULL,
+-- first_name varchar(255),
+-- last_name VARCHAR (255),
+-- PRIMARY KEY (id)
+-- );
 
-INSERT INTO People
-VALUES(0, 'Coby', 'Konkol');
+-- INSERT INTO People
+-- VALUES(0, 'Coby', 'Konkol');
 
-INSERT INTO People
-VALUES(1, 'John', 'Doe');
+-- INSERT INTO People
+-- VALUES(1, 'John', 'Doe');
 
